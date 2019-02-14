6a1714cb5b0fec16d0febae6fe8f9fdafeaa3c47
ConnorQ <32547171+ConnorQ@users.noreply.github.com>
2019-02-14 12:43:53 -0500

    Completed Moving Cat

---
+++ MovingCatConnorQ/MovingCatConnorQ/MovingCatForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace MovingCatConnorQ
+{
+    partial class frmMovingCat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mnuProgram = new System.Windows.Forms.MenuStrip();
+            this.picCat = new System.Windows.Forms.PictureBox();
+            this.mniSwitch = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniCat1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniCat2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuProgram.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picCat)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mnuProgram
+            // 
+            this.mnuProgram.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniSwitch});
+            this.mnuProgram.Location = new System.Drawing.Point(0, 0);
+            this.mnuProgram.Name = "mnuProgram";
+            this.mnuProgram.Size = new System.Drawing.Size(666, 24);
+            this.mnuProgram.TabIndex = 0;
+            this.mnuProgram.Text = "menuStrip1";
+            // 
+            // picCat
+            // 
+            this.picCat.Image = global::MovingCatConnorQ.Properties.Resources.cat2;
+            this.picCat.Location = new System.Drawing.Point(220, 76);
+            this.picCat.Name = "picCat";
+            this.picCat.Size = new System.Drawing.Size(193, 198);
+            this.picCat.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCat.TabIndex = 1;
+            this.picCat.TabStop = false;
+            // 
+            // mniSwitch
+            // 
+            this.mniSwitch.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniCat1,
+            this.mniCat2});
+            this.mniSwitch.Name = "mniSwitch";
+            this.mniSwitch.Size = new System.Drawing.Size(90, 20);
+            this.mniSwitch.Text = "Switch Image";
+            // 
+            // mniCat1
+            // 
+            this.mniCat1.Name = "mniCat1";
+            this.mniCat1.Size = new System.Drawing.Size(152, 22);
+            this.mniCat1.Text = "Cat1";
+            this.mniCat1.Click += new System.EventHandler(this.mniCat1_Click);
+            // 
+            // mniCat2
+            // 
+            this.mniCat2.Name = "mniCat2";
+            this.mniCat2.Size = new System.Drawing.Size(152, 22);
+            this.mniCat2.Text = "Cat2";
+            this.mniCat2.Click += new System.EventHandler(this.mniCat2_Click);
+            // 
+            // frmMovingCat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 359);
+            this.Controls.Add(this.picCat);
+            this.Controls.Add(this.mnuProgram);
+            this.MainMenuStrip = this.mnuProgram;
+            this.Name = "frmMovingCat";
+            this.Text = "Moving Cat By Connor Quinlan";
+            this.mnuProgram.ResumeLayout(false);
+            this.mnuProgram.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picCat)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnuProgram;
+        private System.Windows.Forms.PictureBox picCat;
+        private System.Windows.Forms.ToolStripMenuItem mniSwitch;
+        private System.Windows.Forms.ToolStripMenuItem mniCat1;
+        private System.Windows.Forms.ToolStripMenuItem mniCat2;
+    }
+}
+
---
+++ MovingCatConnorQ/MovingCatConnorQ/MovingCatForm.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Created by: Connor Quinlan
+ * Created on: 14-02-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #9 - Moving Cat
+ * This program gives the user the option to switch between 2 images of a cat.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovingCatConnorQ
+{
+    public partial class frmMovingCat : Form
+    {
+        public frmMovingCat()
+        {
+            InitializeComponent();
+        }
+
+        private void mniCat1_Click(object sender, EventArgs e)
+        {
+            this.picCat.Image = Properties.Resources.cat1;
+        }
+
+        private void mniCat2_Click(object sender, EventArgs e)
+        {
+            this.picCat.Image = Properties.Resources.cat2;
+        }
+    }
+}
