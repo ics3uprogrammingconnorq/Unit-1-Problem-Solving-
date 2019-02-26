309c6568bf93ba53830a521bfdabf9aad66551cb
ConnorQ <32547171+ConnorQ@users.noreply.github.com>
2019-02-26 11:55:02 -0500

    pizza cost

---
+++ PizzaCostConnorQ/PizzaCostConnorQ/PizzaCostForm.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace PizzaCostConnorQ
+{
+    partial class frmPizzaCost
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblInches = new System.Windows.Forms.Label();
+            this.lstInput = new System.Windows.Forms.ListBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblSubtotal = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(278, 49);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(151, 37);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Pizza Cost";
+            // 
+            // lblInches
+            // 
+            this.lblInches.AutoSize = true;
+            this.lblInches.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInches.Location = new System.Drawing.Point(40, 177);
+            this.lblInches.Name = "lblInches";
+            this.lblInches.Size = new System.Drawing.Size(363, 23);
+            this.lblInches.TabIndex = 1;
+            this.lblInches.Text = "Enter the diameter in inches of the pizza: ";
+            // 
+            // lstInput
+            // 
+            this.lstInput.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstInput.FormattingEnabled = true;
+            this.lstInput.ItemHeight = 23;
+            this.lstInput.Items.AddRange(new object[] {
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.lstInput.Location = new System.Drawing.Point(421, 177);
+            this.lstInput.Name = "lstInput";
+            this.lstInput.Size = new System.Drawing.Size(105, 27);
+            this.lstInput.TabIndex = 2;
+            this.lstInput.SelectedIndexChanged += new System.EventHandler(this.lstInput_SelectedIndexChanged);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(59, 239);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblSubtotal
+            // 
+            this.lblSubtotal.AutoSize = true;
+            this.lblSubtotal.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubtotal.Location = new System.Drawing.Point(41, 308);
+            this.lblSubtotal.Name = "lblSubtotal";
+            this.lblSubtotal.Size = new System.Drawing.Size(21, 23);
+            this.lblSubtotal.TabIndex = 4;
+            this.lblSubtotal.Text = "?";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(41, 370);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(21, 23);
+            this.lblTotal.TabIndex = 5;
+            this.lblTotal.Text = "?";
+            // 
+            // frmPizzaCost
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 478);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblSubtotal);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lstInput);
+            this.Controls.Add(this.lblInches);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "frmPizzaCost";
+            this.Text = "Pizza Cost by Connor Quinlan";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblInches;
+        private System.Windows.Forms.ListBox lstInput;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblSubtotal;
+        private System.Windows.Forms.Label lblTotal;
+    }
+}
+
---
+++ PizzaCostConnorQ/PizzaCostConnorQ/PizzaCostForm.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Created by: Connor Quinlan
+ * Created on: 22-02-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #12 - Pizza Cost
+ * This program calculates the price of a pizza based on given diameter.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PizzaCostConnorQ
+{
+    public partial class frmPizzaCost : Form
+    {
+        // Declair Variables
+        double diameter = 5;
+        double subtotal;
+        double total;
+
+        public frmPizzaCost()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // Calculate and diplay price
+            subtotal = diameter * 0.5 + 0.75 + 0.99;
+            total = subtotal * 1.13;
+            subtotal = Math.Round(subtotal * 100) / 100;
+            total = Math.Round(total * 100) / 100;
+            lblSubtotal.Text = "Subtotal: $" + Convert.ToString(subtotal);
+            lblTotal.Text = "Total: $" + Convert.ToString(total);
+        }
+
+        private void lstInput_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // When box is changed set diameter and reset prices
+            diameter = Convert.ToDouble(lstInput.SelectedItem);
+            lblSubtotal.Text = "?";
+            lblTotal.Text = "?";
+        }
+    }
+}
