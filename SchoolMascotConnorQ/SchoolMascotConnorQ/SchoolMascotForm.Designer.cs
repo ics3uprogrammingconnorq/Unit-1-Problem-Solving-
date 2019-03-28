d9282368f21e2a09c2b39176cc2c12bc4a237e39
ConnorQ <32547171+ConnorQ@users.noreply.github.com>
2019-03-28 18:29:24 -0400

    mascot

--- SchoolMascotConnorQ/SchoolMascotConnorQ/SchoolMascotForm.Designer.cs
+++ SchoolMascotConnorQ/SchoolMascotConnorQ/SchoolMascotForm.Designer.cs
@@ -35,7 +35,12 @@
             this.mniInterSchoolOfMalaysia = new System.Windows.Forms.ToolStripMenuItem();
             this.mniInterSchoolOfKenya = new System.Windows.Forms.ToolStripMenuItem();
             this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.grbSchool = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.lblSchool = new System.Windows.Forms.Label();
+            this.lblMascot = new System.Windows.Forms.Label();
             this.mnuSchools.SuspendLayout();
+            this.grbSchool.SuspendLayout();
             this.SuspendLayout();
             // 
             // mnuSchools
@@ -65,24 +70,28 @@
             this.mniImmaculata.Name = "mniImmaculata";
             this.mniImmaculata.Size = new System.Drawing.Size(245, 22);
             this.mniImmaculata.Text = "Immaculata";
+            this.mniImmaculata.Click += new System.EventHandler(this.mniImmaculata_Click);
             // 
             // mniStMarks
             // 
             this.mniStMarks.Name = "mniStMarks";
             this.mniStMarks.Size = new System.Drawing.Size(245, 22);
             this.mniStMarks.Text = "St.Marks";
+            this.mniStMarks.Click += new System.EventHandler(this.mniStMarks_Click);
             // 
             // mniInterSchoolOfMalaysia
             // 
             this.mniInterSchoolOfMalaysia.Name = "mniInterSchoolOfMalaysia";
             this.mniInterSchoolOfMalaysia.Size = new System.Drawing.Size(245, 22);
             this.mniInterSchoolOfMalaysia.Text = "International School Of Malaysia";
+            this.mniInterSchoolOfMalaysia.Click += new System.EventHandler(this.mniInterSchoolOfMalaysia_Click);
             // 
             // mniInterSchoolOfKenya
             // 
             this.mniInterSchoolOfKenya.Name = "mniInterSchoolOfKenya";
             this.mniInterSchoolOfKenya.Size = new System.Drawing.Size(245, 22);
             this.mniInterSchoolOfKenya.Text = "International School Of Kenya";
+            this.mniInterSchoolOfKenya.Click += new System.EventHandler(this.mniInterSchoolOfKenya_Click);
             // 
             // mniExit
             // 
@@ -91,17 +100,50 @@
             this.mniExit.Text = "Exit";
             this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
             // 
+            // grbSchool
+            // 
+            this.grbSchool.Controls.Add(this.lblMascot);
+            this.grbSchool.Controls.Add(this.lblSchool);
+            this.grbSchool.Location = new System.Drawing.Point(57, 110);
+            this.grbSchool.Name = "grbSchool";
+            this.grbSchool.Size = new System.Drawing.Size(741, 258);
+            this.grbSchool.TabIndex = 1;
+            this.grbSchool.TabStop = false;
+            // 
+            // lblSchool
+            // 
+            this.lblSchool.AutoSize = true;
+            this.lblSchool.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSchool.Location = new System.Drawing.Point(40, 42);
+            this.lblSchool.Name = "lblSchool";
+            this.lblSchool.Size = new System.Drawing.Size(33, 37);
+            this.lblSchool.TabIndex = 0;
+            this.lblSchool.Text = "?";
+            // 
+            // lblMascot
+            // 
+            this.lblMascot.AutoSize = true;
+            this.lblMascot.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMascot.Location = new System.Drawing.Point(40, 155);
+            this.lblMascot.Name = "lblMascot";
+            this.lblMascot.Size = new System.Drawing.Size(33, 37);
+            this.lblMascot.TabIndex = 1;
+            this.lblMascot.Text = "?";
+            // 
             // frmSchoolMascot
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(849, 521);
+            this.Controls.Add(this.grbSchool);
             this.Controls.Add(this.mnuSchools);
             this.MainMenuStrip = this.mnuSchools;
             this.Name = "frmSchoolMascot";
             this.Text = "School Mascot By Connor Quinlan";
             this.mnuSchools.ResumeLayout(false);
             this.mnuSchools.PerformLayout();
+            this.grbSchool.ResumeLayout(false);
+            this.grbSchool.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -116,6 +158,10 @@
         private System.Windows.Forms.ToolStripMenuItem mniInterSchoolOfMalaysia;
         private System.Windows.Forms.ToolStripMenuItem mniInterSchoolOfKenya;
         private System.Windows.Forms.ToolStripMenuItem mniExit;
+        private System.Windows.Forms.GroupBox grbSchool;
+        private System.Windows.Forms.Label lblMascot;
+        private System.Windows.Forms.Label lblSchool;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
--- SchoolMascotConnorQ/SchoolMascotConnorQ/SchoolMascotForm.cs
+++ SchoolMascotConnorQ/SchoolMascotConnorQ/SchoolMascotForm.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Created by: Connor Quinlan
+ * Created on: Day-Month-Year
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #8 - School Mascot
+ * This program displays the school name and mascot of each in the menu strip.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,6 +28,30 @@ namespace SchoolMascotConnorQ
         private void mniExit_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void mniImmaculata_Click(object sender, EventArgs e)
+        {
+            lblSchool.Text = "Immaculata";
+            lblMascot.Text = "Saints";
+        }
+
+        private void mniStMarks_Click(object sender, EventArgs e)
+        {
+            lblSchool.Text = "Lisgar";
+            lblMascot.Text = "Lords";
+        }
+
+        private void mniInterSchoolOfMalaysia_Click(object sender, EventArgs e)
+        {
+            lblSchool.Text = "International School of Malaysia";
+            lblMascot.Text = "Eagles";
+        }
+
+        private void mniInterSchoolOfKenya_Click(object sender, EventArgs e)
+        {
+            lblSchool.Text = "International School of Kenya";
+            lblMascot.Text = "Lions";
         }
     }
 }
